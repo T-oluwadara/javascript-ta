8af74a705ed2535c61a768b58cc5645375c4cdb0
T-oluwadara <105435815+T-oluwadara@users.noreply.github.com>
2022-06-07 12:27:07 -0700

    Answered test question
    This is a push of my answers to the test question

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // complete the function
-function prime (num) {
-  // code goes here
+function prime(num) {
+  for (var i = 2; i < num; i++)
+    if (num % i === 0) return false;
+  return num > 1;
 }
 
-function solution (arg) {
+function solution(arg) {
   return prime(arg);
 }
 
